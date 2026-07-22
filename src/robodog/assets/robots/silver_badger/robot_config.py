2a3d59bb6a291e8fb563ed68daafac181c4e186a
Adam Szułczyński <szulczynskiadam31@gmail.com>
2026-07-22 14:10:18 +0000

    Silver badger

---
+++ src/robodog/assets/robots/silver_badger/robot_config.py
@@ -0,0 +1,16 @@
+robot_config = {
+    "short_name": "sbg",
+    "spine_locked": True,
+
+    "actuator_joint_max_velocities": [
+        3.15,
+        25.0, 25.0, 25.0,
+        25.0, 25.0, 25.0,
+        25.0, 25.0, 25.0,
+        25.0, 25.0, 25.0
+    ],
+
+    "scaling_factor": 0.25,
+
+    "actuator_joints_to_stay_near_nominal": []
+}
